f977d6dcdefc4e3a612121c14379b93e47f84a6d
nishikei <nishikei@amazon.co.jp>
2016-04-04 11:14:00 +0900

    Externalize common function

--- script/devcon.js
+++ script/devcon.js
@@ -163,26 +163,6 @@ ReactDOM.render(
   );
 
 
-function truncate(node){
-  var location = ''
-  if(node.mark.substr(0,1) == 'P'){
-    location = node.mark.substr(1,2);
-  } else {
-    location = node.mark.substr(1,1);
-  }
-  return {
-    type: node.mark.substr(0,1),
-    title: node.name, 
-    description: node.description, 
-    time: node.start, 
-    key: node.mark, 
-    location: location, 
-    slot: node.mark.substr(node.mark.length - 4, 4)
-  };
-}
-
-
-
 
 
 
